7eec64b5170e23aa4932754343a7732ab7153a9f
jalex141 <saiz30@gmail.com>
2021-08-29 14:22:20 +0200

    s

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import numpy as np
-import re
-import os
 import sys
 sys.path.append('../')
 import src.cleaning as cl
@@ -10,8 +6,27 @@ import src.cleaning as cl
 #import data from kaggle
 cl.download_dataset()
 
-#load dataset
+#load datasets
 pl = pd.read_csv('./data/openpowerlifting.csv',encoding="ISO-8859-1")
+m = pd.read_csv('./data/meets.csv',encoding="ISO-8859-1")
 
+#fix column names
+cl.fix_col(pl)
+
+#dron NaN columns and rows
 cl.nan(pl)
+
+#Drop columns with scarce data
 cl.drop(pl)
+
+#remove simultaneous division entries
+cl.division(pl)
+
+#merge date from another data frame 
+cl.date(pl,m)
+
+#fill NaN acording to dtype
+cl.filna(pl)
+
+#export data frame
+pl.to_csv ('./data/pl.csv')
--- src/cleaning.py
+++ src/cleaning.py
@@ -34,6 +34,13 @@ def download_dataset():
     move_and_delete = f"mv *.csv ./data/data_set.csv"
     return os.system(move_and_delete)
 
+#fix col names
+def fix_col(df):
+    """fixes column names so thet can be used in functions without issues"""
+    diccio_todas = {col: col.replace(" ","_")  for col in list(df.columns)}
+    df.rename(columns=diccio_todas, inplace=True)
+    return df
+
 def nan(df):
     """takes a data frame and removes all nan rows and columns"""
     df.dropna(axis=0, how = "all",inplace=True)
@@ -44,9 +51,36 @@ def drop(df):
     """asks for user autorization to drop columns with scarse data"""
     percent_missing = round(df.isnull().sum() * 100 / len(df), 2)
     if input("drop poor data colums? [y/n] ")=="y":
+        t=int(input("Threshold [%] for data "))
         for c,n in zip(percent_missing.index,percent_missing.values):
-            if n>70:
+            if n>t:
                 df.drop([c], axis=1, inplace=True)
     return df
 
+def date(df,df2):
+    """merges with another data frame to get the date for every entry"""
+    df2_d=df2[["MeetID","Date"]]
+    df = df.merge(df2_d, on="MeetID")
+    return df
 
+
+def filna(df):
+    """takes a dataframe and replaces all NaN values depending on the type of the column
+    if type is  string or object replaces with UNK 
+    if type is int or float returns -1"""
+    col_n = list(df.select_dtypes(include="object").columns) 
+    for col in list(df.columns):
+        if col in col_n:
+            df.fillna("UNK",inplace=True)
+        else:
+            df.fillna(-1,inplace=True)
+    return df
+
+def division(df):
+    """there are some divisions running simultaneously this gets rid of one of them"""
+    pl_d=df["Division"]
+    df.drop(["Division"], axis=1,inplace=True)
+    df.drop_duplicates(inplace=True)
+    #recover the division column
+    df = df.merge(pl_d, left_index=True, right_index=True)
+    return df
